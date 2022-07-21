b83365d6b229b78d06b5a87870a9332660fcabeb
tzq0301 <1661610563@qq.com>
2022-07-22 01:46:34 +0800

    完成注册接口

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sqlalchemy.exc
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from db import User, get_db
-from model import LoginInfo
+from model import LoginInfo, RegisterInfo
 
 app: FastAPI = FastAPI()
 
@@ -20,3 +21,16 @@ app.add_middleware(
 async def login(login_info: LoginInfo, db: Session = Depends(get_db)) -> bool:
     return db.query(User).filter(User.username == login_info.username).filter(
         User.password == login_info.password).one_or_none() is not None
+
+
+@app.post("/register")
+async def register(register_info: RegisterInfo, db: Session = Depends(get_db)) -> bool:
+    user = User()
+    user.username = register_info.username
+    user.password = register_info.password
+    db.add(user)
+    try:
+        db.commit()
+    except sqlalchemy.exc.IntegrityError:
+        return False
+    return True
--- model.py
+++ model.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class LoginInfo(BaseModel):
     username: str
     password: str
+
+
+class RegisterInfo(BaseModel):
+    username: str
+    password: str
